1e463a143501779f046457cc7a283ee5ba685c47
Chunarev_Sergey <sergey.chunarev@ural.rt.ru>
2023-06-29 14:32:32 +0200

    init

---
+++ src/main/java/com/smarthome/sheurekaserver/ShEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.smarthome.sheurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ShEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShEurekaServerApplication.class, args);
+	}
+
+}
